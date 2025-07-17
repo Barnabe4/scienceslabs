3c2faaff89e94fc238fd0aca50bb78ddb84928a2
barnabe3 <hounkpatibarnabe@yahoo.fr>
2025-07-17 19:01:04 +0000

    Fix missing Link import in Services page

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Truck, HeadphonesIcon, Award, Users, BookOpen, Wrench, Shield, Globe } from 'lucide-react';
 
 const Services = () => {
